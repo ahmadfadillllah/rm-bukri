a77c95fc7cecf74a88a73d3bc0447b0f67c358ea
ahmadfadillllah <ahmadfadillah502@gmail.com>
2022-12-18 21:42:26 +0800

    First

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Configuration Database
+ */
+return array(
+    'host' => "",
+    'username' => "root",
+    'password' => "",
+    'dbname' => "bukri2",
+    // 'port' => "", // sementara menggunakan port bawaan
+);
+
+?>
